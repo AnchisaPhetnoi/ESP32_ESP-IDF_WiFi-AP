320e672670876844576e49c26f4ee09e20bf0674
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-09-19 14:46:19 +0700

    ST2567_ESP32_WiFi-AP

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "E:/Special_Topics_2567/ST2567_ESP32_WiFi-AP/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/admin/esp/v5.2.2/esp-idf/.git")
+set(GIT_DIR "E:/Special_Topics_2567/ST2567_ESP32_WiFi-AP/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
